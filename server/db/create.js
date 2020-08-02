9ee2d1fd66eea9af1e705db363d68eb58dd1f10b
Matt Elhotiby <hotiby@gmail.com>
2020-08-02 12:28:48 -0400

    Handle errors in the create db script

--- server/db/create.js
+++ server/db/create.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv')
+const KNEX = require('knex')
 
 // Load env vars
 dotenv.config()
@@ -6,7 +7,7 @@ dotenv.config()
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_PORT } = process.env
 
 async function createDatabase() {
-  const knex = require('knex')({
+  const knex = KNEX({
     client: 'pg',
     connection: {
       host: DB_HOST,
@@ -17,9 +18,14 @@ async function createDatabase() {
     }
   })
 
-  await knex.raw(`CREATE DATABASE ${DB_DATABASE}`)
-  console.log(`Created database ${DB_DATABASE}`)
-  await knex.destroy()
+  try {
+    await knex.raw(`CREATE DATABASE ${DB_DATABASE}`)
+    console.log(`Created database ${DB_DATABASE}`)
+  } catch (e) {
+    console.log(e.message)
+  } finally {
+    await knex.destroy()
+  }
 }
 
 createDatabase()
